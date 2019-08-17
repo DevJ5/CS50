4da5f70ab5fc3d23285dab91b4f007e0eb4fc968
DevJ5 <idevelopjs@gmail.com>
2019-08-17 11:59:37 +0200

    Define statement

--- reference.c
+++ reference.c
@@ -1,8 +1,9 @@
-// #include <stdio.h>
-// #include <stdlib.h>
-// #include <string.h>
-// #include <ctype.h>
-// #include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <math.h>
+#define COUNT 10
 
 // isalpha
 // isdigit
