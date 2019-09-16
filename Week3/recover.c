b1eb9c589b8286d73a8f1d152a0569246d9924c8
DevJ5 <idevelopjs@gmail.com>
2019-09-16 21:33:14 +0200

    Recover.c

---
+++ Week3/recover.c
@@ -0,0 +1,142 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int foundFirstJPEG(FILE *inputFile);
+void writeToFile(FILE *inputFile, FILE *outputFile);
+int checkIsEndOfJPEG(FILE *inputFile);
+
+int main(int argc, char *argv[])
+{
+    // 1 command line argument, the name of the forensic image                                      [x]
+    // error handling for > 1 argument. print to stderr, main returns 1                             [x]
+    // if the forensic image cannot be opened for reading, fprintf to stderr, main returns 2        [x]
+    // if using malloc, must be freed                                                               [ ]
+
+    // Check if there is 1 command line argument
+    if (argc != 2)
+    {
+        fprintf(stderr, "Usage: ./recover image\n");
+        return 1;
+    }
+
+    // Check if the forensic image can be opened for reading
+    FILE *inputFile = fopen(argv[1], "r");
+    if (inputFile == NULL)
+    {
+        fprintf(stderr, "Usage: ./recover image\n");
+        return 2;
+    }
+
+    // Find the first JPEG
+    int foundFirst = foundFirstJPEG(inputFile);
+
+    if (foundFirst)
+    {
+        // Create new JPEG file
+        FILE *outputFile = fopen("000.jpg", "w");
+        printf("We write from: %ld\n", ftell(inputFile));
+        writeToFile(inputFile, outputFile);
+        printf("We are now at: %ld\n", ftell(inputFile));
+
+        while (1)
+        {
+            // Check if we reached the end of a JPEG
+            if (!checkIsEndOfJPEG(inputFile))
+            {
+                writeToFile(inputFile, outputFile);
+                printf("This should print now: %ld\n", ftell(inputFile));
+            }
+            else
+            {
+                break;
+            }
+        }
+    }
+
+    fclose(inputFile);
+}
+
+// --- End of main --- //
+
+int foundFirstJPEG(FILE *inputFile)
+{
+    unsigned char c0 = fgetc(inputFile);
+    printf("%ld: %x\n", ftell(inputFile), c0);
+    if (c0 == 0xff)
+    {
+        unsigned char c1 = fgetc(inputFile);
+        if (c1 == 0xd8)
+        {
+            unsigned char c2 = fgetc(inputFile);
+            if (c2 == 0xff)
+            {
+                unsigned char c3 = fgetc(inputFile);
+                // If this 4th byte is correct, we have identified the start of a JPEG
+                if (c3 >= 0xe0 || c3 <= 0xef)
+                {
+                    // Put the pointer back to the first byte of the JPEG
+                    fseek(inputFile, -4, SEEK_CUR);
+                    return 1;
+                }
+            }
+        }
+    }
+
+    // Return 0 if we reached the end of the file without finding a JPEG
+    if (feof(inputFile))
+    {
+        return 0;
+    }
+
+    return foundFirstJPEG(inputFile);
+}
+
+void writeToFile(FILE *inputFile, FILE *outputFile)
+{
+    // Define character block of 512 bytes
+    char bytes[512];
+    // Read a block of 512 bytes
+    fread(bytes, 1, 512, inputFile);
+    // Write a block of 512 bytes
+    fwrite(bytes, 1, 512, outputFile);
+
+    return;
+}
+
+int checkIsEndOfJPEG(FILE *inputFile)
+{
+    unsigned char c0 = fgetc(inputFile);
+    if (c0 == 0xff)
+    {
+        unsigned char c1 = fgetc(inputFile);
+        if (c1 == 0xd8)
+        {
+            unsigned char c2 = fgetc(inputFile);
+            if (c2 == 0xff)
+            {
+                unsigned char c3 = fgetc(inputFile);
+                // If this 4th byte is correct, we have identified the start of a new JPEG
+                if (c3 >= 0xe0 || c3 <= 0xef)
+                {
+                    // Put the pointer back to the first byte of the JPEG
+                    fseek(inputFile, -4, SEEK_CUR);
+                    return 1;
+                }
+            }
+            else
+            {
+                fseek(inputFile, -3, SEEK_CUR);
+            }
+        }
+        else
+        {
+            fseek(inputFile, -2, SEEK_CUR);
+        }
+    }
+    else
+    {
+        fseek(inputFile, -1, SEEK_CUR);
+    }
+
+    return 0;
+}
