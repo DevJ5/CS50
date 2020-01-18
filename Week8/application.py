d999691ac1ba872fa50d35d1f4f490226c4bfb0d
DevJ5 <idevelopjs@gmail.com>
2020-01-18 13:52:14 +0100

    Week 8

--- Week8/application.py
+++ Week8/application.py
@@ -1,6 +1,7 @@
 import os
 
-from cs50 import SQL
+# from cs50 import SQL
+import sqlite3
 from flask import Flask, flash, jsonify, redirect, render_template, request, session
 from flask_session import Session
 from tempfile import mkdtemp
@@ -23,7 +24,6 @@ def after_request(response):
     response.headers["Pragma"] = "no-cache"
     return response
 
-
 # Custom filter
 app.jinja_env.filters["usd"] = usd
 
@@ -31,10 +31,13 @@ app.jinja_env.filters["usd"] = usd
 app.config["SESSION_FILE_DIR"] = mkdtemp()
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
-Session(app)
+# Session(app)
+app.secret_key = b'A'
 
 # Configure CS50 Library to use SQLite database
-db = SQL("sqlite:///finance.db")
+# db = SQL("sqlite:///finance.db")
+conn = sqlite3.connect('finance.db', check_same_thread=False)
+cur = conn.cursor()
 
 # Make sure API key is set
 if not os.environ.get("API_KEY"):
@@ -77,29 +80,25 @@ def login():
 
     # User reached route via POST (as by submitting a form via POST)
     if request.method == "POST":
-
+        username = request.form.get("username")
+        password = request.form.get("password")
         # Ensure username was submitted
-        if not request.form.get("username"):
+        if not username:
             return apology("must provide username", 403)
 
         # Ensure password was submitted
-        elif not request.form.get("password"):
+        elif not password:
             return apology("must provide password", 403)
 
         # Query database for username
-        rows = db.execute(
-            "SELECT * FROM users WHERE username = :username",
-            username=request.form.get("username"),
-        )
-
+        rows = cur.execute("SELECT * FROM users WHERE username =?", (username,)).fetchall()
+        print(rows)
         # Ensure username exists and password is correct
-        if len(rows) != 1 or not check_password_hash(
-            rows[0]["hash"], request.form.get("password")
-        ):
+        if len(rows) != 1 or not check_password_hash(rows[0][2], password):
             return apology("invalid username and/or password", 403)
 
         # Remember which user has logged in
-        session["user_id"] = rows[0]["id"]
+        session["user_id"] = rows[0][0]
 
         # Redirect user to home page
         return redirect("/")
@@ -124,13 +123,35 @@ def logout():
 @login_required
 def quote():
     """Get stock quote."""
-    return apology("TODO")
-
+    if request.method == "GET":
+        return render_template("quote.html")
+    else:
+        symbol = request.form['symbol']
+        quote = lookup(symbol)
+        print(quote)
+        print(type(quote))
+        return render_template("quoted.html", quote=quote)
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
     """Register user"""
-    return apology("TODO")
+    if request.method == "POST":
+        username = request.form['username']
+        password = request.form['password']
+        confirmation = request.form['confirmation']
+
+        # Get username from database
+        rows = cur.execute(f"select * from users where users.username =?", (username,))
+        # Check if username doesnt exist yet and password input is correct
+        if rows.fetchone() == None and password == confirmation:
+            hashedPassword = generate_password_hash(password, method='pbkdf2:sha256', salt_length=8)
+            # Create new user in the database
+            cur.execute("INSERT INTO users (username, hash) VALUES (?, ?)", (username, hashedPassword))
+            conn.commit()
+
+        return redirect("/login")
+    else:
+        return render_template("signup.html")
 
 
 @app.route("/sell", methods=["GET", "POST"])
